e6ac02c7b4a796ac79b5a3bda587b3089ce28897
ASUS <rukshaninduwara123@gmail.com>
2024-09-02 15:49:47 +0530

    Initial Setup for Spring Boot Backend

---
+++ src/test/java/com/example/ehr_backend/EhrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ehr_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
